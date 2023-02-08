51910a7573f65440504ff128bc675e0f388dfa8d
KYUNGMINNA <nkm4095@naver.com>
2023-02-08 13:29:16 +0900

    # 회원가입

---
+++ NewJeans/src/main/java/com/example/NewJeans/dto/request/SignUpRequestDTO.java
@@ -0,0 +1,31 @@
+package com.example.NewJeans.dto.request;
+
+import com.example.NewJeans.entity.Member;
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class SignUpRequestDTO {
+    @NotBlank
+    @Email
+    private String memEmail;
+    @NotBlank
+    @Size(min = 8,max = 20)
+    private String memPassword;
+
+    public Member toEntity(){
+        return Member.builder()
+                .memEmail(this.memEmail)
+                .memPassword(this.memPassword)
+                .build();
+    }
+}
