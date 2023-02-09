08e1b869b28f207788f5584465ffa253d6947944
ds02168 <1995musso@gmail.com>
2023-02-09 11:24:22 +0900

    # 회원 인증 처리 추가

---
+++ NewJeans/src/main/java/com/example/NewJeans/security/auth/LoginDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.NewJeans.security.auth;
+
+import com.example.NewJeans.entity.MemberShip;
+import com.example.NewJeans.repository.MemberShipRepository;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginDetailsService implements UserDetailsService {
+    private final MemberShipRepository memberShipRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Long memId = Long.getLong(username);
+        MemberShip memberShip = memberShipRepository.findByMem_MemID(memId);
+        if(memberShip != null){
+            return new LoginDetails(memberShip);
+        }
+        return null;
+    }
+}
